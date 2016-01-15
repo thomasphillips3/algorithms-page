35a5ae5bfc7f2f5cde63ab4ade9420c0bdd87a88
Thomas Phillips <tpphilli@amazon.com>
2016-01-14 23:47:35 -0800

    Changed the id name for absolute value's text input Added primality section

--- algorithms.js
+++ algorithms.js
@@ -1,5 +1,5 @@
 function abs(){
-  var x = parseInt(document.getElementById("number").value);
+  var x = parseInt(document.getElementById("absolute").value);
   if (isNaN(x)){
     document.getElementById("abs").innerHTML = "Invalid input";
   } else if (x<0){
@@ -10,5 +10,22 @@ function abs(){
     document.getElementById("abs").innerHTML = "abs(" + x + ") = " + x;
     console.log("#abs(" + x + ") = " + x);
     // $("abs").show();
+  }
+}
+
+function prime(){
+  var x = parseInt(document.getElementById("primality").value);
+  if(isNaN(x)){
+    document.getElementById("prime").innerHTML = "Invalid input. Enter a number.";
+  } else if (x<2){
+    document.getElementById("prime").innerHTML = "Invalid input. Enter a number greater than 2.";
+  } else {
+    for (var i=2; (i*i <= x); i++){
+      if(x % i == 0){
+        document.getElementById("prime").innerHTML = x + " is not prime.";
+      } else{
+        document.getElementById("prime").innerHTML = x + " is prime.";
+      }
+    }
   }
 }
