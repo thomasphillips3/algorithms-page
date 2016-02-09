9b981f47c141a0f52ab854ce5f0f815f6cb9a100
Thomas Phillips <tpphilli@amazon.com>
2016-02-08 23:09:01 -0800

    Solved

--- js/palindrome.js
+++ js/palindrome.js
@@ -1,1 +1,20 @@
-(function(){console.log("Hey IIFE");}());
+(function(){
+  console.log("Hey IIFE");
+  var btnCheckPalindrome = document.getElementById("checkPalindrome");
+  var elResults = document.getElementById("pal");
+  var elInput = document.getElementById("str");
+  btnCheckPalindrome.addEventListener("click", checkPalindrome, false);
+
+  function checkPalindrome() {
+    elResults.style.visibility = "visible";
+    str = elResults.innerHTML = elInput.value;
+    str = str.replace(/[^A-Z0-9]/ig, "").toLowerCase();
+    if (str === str.split('').reverse().join('')){
+      elResults.innerHTML = "Palindrome";
+    }
+    else {
+      elResults.innerHTML = "Not a palindrome";
+    }
+    // console.log(elInput.value);
+  }
+}());
